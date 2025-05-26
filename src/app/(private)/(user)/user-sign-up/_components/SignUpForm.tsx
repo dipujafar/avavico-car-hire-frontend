abe80c959a8061fbcb37d8688c396ae5bdd44a64
dipujafar <dipujafarmr@gmail.com>
2025-05-26 07:53:25 +0600

    some chamge

--- src/app/(private)/(user)/user-sign-up/_components/SignUpForm.tsx
+++ src/app/(private)/(user)/user-sign-up/_components/SignUpForm.tsx
@@ -68,7 +68,7 @@ const SignUpForm = () => {
     },
   });
 
-  const onSubmit = (data: z.infer<typeof formSchema>) => {
+const onSubmit = (data: z.infer<typeof formSchema>) => {
     console.log(data);
   };
 
