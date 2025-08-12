10846caca6a76b5a5dbf119f7b0f491eb7dfbb40
dipujafar <dipujafarmr@gmail.com>
2025-08-12 18:40:12 +0600

    comment up translation part

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -135,7 +135,7 @@ export default function Navbar() {
                   )}
                 </div>
               ))}
-              {/* <LanguageSwitcher /> */}
+              <LanguageSwitcher />
             
             </nav>
 
